562dd2bb710e635b82a1f4565e25148b0407ea82
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-02-17 19:24:40 +0700

    file to push heroku

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:7699';
+  const ENDPOINT = 'https://chatroomvn.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
