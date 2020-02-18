70c4dbf9b0071a961212e9d8224a3b6f9e98b2e8
Trần Ngọc Quang <56824077+tranngoc769@users.noreply.github.com>
2020-02-18 10:42:47 +0700

    comment in git ignore
    up build and netlify to git

---
+++ client/build/precache-manifest.2c66a9e138a68a615b05d7bfb9fadc9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8f4d568956b95fc97d063dc2ddc2e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f19692885e86607031ed",
+    "url": "/static/css/main.fcccfd08.chunk.css"
+  },
+  {
+    "revision": "2522e3fba2811995bad9",
+    "url": "/static/js/2.91c90077.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/static/js/2.91c90077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f19692885e86607031ed",
+    "url": "/static/js/main.f7487346.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "cdee81174a5647ef49e01929fca6d7fc",
+    "url": "/static/media/background.cdee8117.jpg"
+  }
+]);
